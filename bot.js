ba4be33d658feddc4224d9453bfb851f13997f6a
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2021-08-08 01:06:34 +0700

    before bedtime

--- bot.js
+++ bot.js
@@ -1,28 +1,30 @@
-'use strict'
-const Discord = require('discord.js');
-// Create a Json file which contain ur discord bot Token and it's prefix
-const Prefix = "$"
-const client = new Discord.Client();
 const fs = require('fs');
 const ytdl = require('ytdl-core');
-require('dotenv').config();
-let prefix = Prefix;
+const Discord = require('discord.js');
+const client = new Discord.Client();
+// Create a Json file which would contain ur discord bot Token and it's prefix
+
+const Prefix = "$";       // Default prefix
+let prefix = Prefix;      // Set prefix
+
+require('dotenv').config();// .env file which contain Token
 
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag);
-    client.channels.cache.get("817064482714419230").send("Cường đang Online trong này!")
+    client.channels.cache.get("817064482714419230").send("Cường đang Online trong này!");
 })
 
+//----------------Listening------------------
 client.on('message', msg => {
-    // logging ev
+    // logging everything from server
     console.log(msg.member.displayName+ ': "'+msg.content+'" in '+msg.channel.name);
-
+    // Prefix checking
     if(msg.content == '<@!'+client.user.id+'>' || msg.content == '<@'+client.user.id+'>'){
         msg.channel.send("Prefix: "+prefix);
         msg.channel.send("Created by Hi There with LOVE <3");
     }
-
+    // Start with the right prefix
     if(!msg.content.startsWith(prefix) || msg.author.bot) return;
     let str = msg.content.substring(prefix.length);
     console.log(str)    // Log everything
@@ -33,31 +35,35 @@ client.on('message', msg => {
 
             break;
         case "ping":
-            msg.channel.send("What's up mah fellow?");
-            msg.channel.send("*Eat my pp!!!*");
-            break;
-        case "zzz":
-            msg.channel.send('@everyone Ngủ đê!!!!');
+            // Ping
+            msg.channel.send("You expected me to reply 'Pong'?\nBut hell no mother fxcker!! F you!!");
             break;
         case "invite":
+            // Invite code
             msg.channel.send('https://discord.com/oauth2/authorize?client_id='+client.user.id+'&scope=bot');
             break;
         case "say":
+            // Repeat exactly what they say
             msg.channel.send(str.substring(arr[0].length))
             break;
         case "prefix":
+            // Working with prefix
             if(msg.author == msg.guild.ownerID && arr.length == 2){
+                // If author is admin
                 console.log("Is Guild-master")
                 if(arr[1] == "default"){
                     prefix = Prefix;
+                    msg.channel.send("Prefix changed to: "+Prefix);
                 }else{
-                    msg.channel.send("Prefix changed to: "+arr[1]);
                     prefix = arr[1];
+                    msg.channel.send("Prefix changed to: "+arr[1]);
                 }
             }else if(msg.author != msg.guild.ownerID && arr.length == 2){
+                // If author is not admin
                 msg.channel.send("You must be the server's admin to change the Prefix");
             }else{
                 msg.channel.send("Prefix: "+prefix);
+                msg.channel.send("Created by Hi There with LOVE <3");
             }
             break;
         case "admin":
@@ -66,8 +72,8 @@ client.on('message', msg => {
                 {
                     client.channels.cache.get(arr[1]).send(str.substring(arr[0].length+arr[1].length+2)) 
                 }else if(arr[1]=="h" || "help"){
-
                     // msg.channel.send()
+
                 }
             }
             break;
@@ -76,4 +82,5 @@ client.on('message', msg => {
     }
 })
 
+//----------------Initialize-------------------
 client.login(process.env.BOT_TOKEN);
